f8ad21663da6a6de65edc0f8cd8ddf7866d55bf9
adeenazara2002 <adinazara2001@gmail.com>
2026-02-26 22:00:57 +0500

    Assignment 01

--- assignment_1.py
+++ assignment_1.py
@@ -25,3 +25,31 @@ import math
 radius = input("Enter the radius of the circle")
 area = math.pi * radius ** 2
 print("The area of the circle is:", area)
+
+# Question 05
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print(lastName + " " + firstName)
+
+# Question 06
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
+
+# Question 07
+# Question 08
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The entered number is even", num)
+
+else:
+    print("The entered number is odd", num)
+    
+    
+    
