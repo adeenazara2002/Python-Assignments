cca9408bff74b10fb6be63fea9dd73ee2de50ebe
adeenazara2002 <adinazara2001@gmail.com>
2026-02-26 21:49:05 +0500

    Assignment 01

---
+++ assignment_1.py
@@ -0,0 +1,27 @@
+# Question 01
+print("""Twinkle, twinkle, little star
+        How I wonder what you are!
+                    Up above the world so high,
+                    Like a diamond in the sky.
+Twinkle, twinkle, little star
+        How I wonder what you are!
+
+""")
+
+# Question 02
+
+import sys 
+print(sys.version)
+
+# Question 03
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+
+# Question 04
+
+import math 
+radius = input("Enter the radius of the circle")
+area = math.pi * radius ** 2
+print("The area of the circle is:", area)
