0d5392a3b2fe2be8b307c06c64d6f5c2f35c079e
Adina Zara <92504513+adeenazara2002@users.noreply.github.com>
2026-02-28 21:46:58 +0500

    Add files via upload

---
+++ Assignment_02.py
@@ -0,0 +1,32 @@
+print("Question 01")
+
+
+accessories = ['Laptop', 'Tablet', 'ipad', 'camera', 'macbook', 'iphone']
+length = len(accessories)
+print('Length of the list is', length)
+
+print("Question 02")
+
+numbers = [10,20,22.5,40,100,110.7]
+numericSum = sum(numbers)
+print("Sum of numeric items in a list", numericSum)
+
+print("Question 03")
+
+
+numbers = [109, 345, 1000, 99999, 10000000, 5647387623289, 0]
+getLargest = max(numbers)
+print("Largest number from a numeric list is", getLargest)
+
+print("Question 04")
+
+
+a = [1,1,2,3,5,8,13,21,34,55,89]
+for list in a:
+    if list < 5:
+      print(list)
+
+
+
+
+
